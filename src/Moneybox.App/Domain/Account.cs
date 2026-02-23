a8490bdfb96b6626018e56653eb4465a8d3ac091
Ben <behnam.iam@gmail.com>
2026-02-23 23:59:05 +0000

    Refactored Transfer

--- src/Moneybox.App/Domain/Account.cs
+++ src/Moneybox.App/Domain/Account.cs
@@ -5,6 +5,8 @@ namespace Moneybox.App
     public class Account
     {
         public const decimal PayInLimit = 4000m;
+        public const decimal LowFundLimit = 500m;
+        public const decimal PayInNotificationThreshold = 500m;
 
         public Guid Id { get; set; }
 
@@ -15,5 +17,29 @@ namespace Moneybox.App
         public decimal Withdrawn { get; set; }
 
         public decimal PaidIn { get; set; }
+
+        public bool HasLowFunds => Balance < LowFundLimit;
+        public bool HasReachedPayInLimit => (PayInLimit - PaidIn) < PayInNotificationThreshold;
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance - amount < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            Balance = Balance - amount;
+            Withdrawn = Withdrawn + amount;
+        }
+
+        public void PayIn(decimal amount)
+        {
+            if (PaidIn + amount > PayInLimit)
+            {
+                throw new InvalidOperationException("Account pay in limit reached");
+            }
+            Balance = Balance + amount;
+            PaidIn = PaidIn + amount;
+        }
     }
 }
